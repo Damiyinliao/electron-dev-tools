1d748654dc8b5d5042ab2eaac3f2e0ec7ce2555a
Damiyinliao <2911493102@qq.com>
2024-06-21 10:48:19 +0800

    feat: 首页

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,7 +1,15 @@
+import { css } from "@emotion/css";
 
 export default function Home() {
+
+  const styles = css`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    height: 100%
+  `
   return (
-    <div>
+    <div className={styles}>
       <h1>💖 Hello World!</h1>
       <p>Welcome to your Electron application.</p>
     </div>
