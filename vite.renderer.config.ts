b2d39c6a4742095e28ffa9cbb1fed5733697f772
Damiyinliao <2911493102@qq.com>
2024-06-21 10:50:16 +0800

    feat: add acro-design & svg-icon plugins

--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -4,6 +4,7 @@ import { pluginExposeRenderer } from './vite.base.config';
 import path from "node:path";
 import React from '@vitejs/plugin-react';
 import { vitePluginForArco } from '@arco-plugins/vite-react';
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
 
 // https://vitejs.dev/config
 export default defineConfig((env) => {
@@ -21,7 +22,12 @@ export default defineConfig((env) => {
     plugins: [
       pluginExposeRenderer(name),
       React(),
-      vitePluginForArco()
+      vitePluginForArco(),
+      createSvgIconsPlugin({
+        iconDirs: [path.resolve(process.cwd(), 'src/assets/svg')], // svg文件夹路径
+        svgoOptions: true, // 启用svgo压缩
+        symbolId: 'icon-[dir]-[name]' // 使用图标文件的名称作为symbol的id
+      })
     ],
     resolve: {
       preserveSymlinks: true,
