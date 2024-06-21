200417b74f9282c2a337dcfeec67435a95d3c72c
Damiyinliao <2911493102@qq.com>
2024-06-21 10:48:35 +0800

    feat: Acro-Design的图标组件

--- src/components/AcroIcon/index.tsx
+++ src/components/AcroIcon/index.tsx
@@ -9,7 +9,8 @@ import {
   IconFolder,
   IconFileImage,
   IconCodeBlock,
-  IconCode
+  IconCode,
+  IconCopy
 } from '@arco-design/web-react/icon'
 
 const iconsMap = {
@@ -23,7 +24,8 @@ const iconsMap = {
   folder: IconFolder,
   fileImage: IconFileImage,
   codeBlock: IconCodeBlock,
-  code: IconCode
+  code: IconCode,
+  copy: IconCopy
 }
 
 type IconName = keyof typeof iconsMap;
@@ -31,15 +33,18 @@ type IconName = keyof typeof iconsMap;
 interface AcroIconProps {
   name: string;
   spin?: boolean;
+  className?: string;
+  style?: React.CSSProperties;
+  onClick?: (e: React.MouseEvent<SVGSVGElement, MouseEvent>) => void;
 }
 
-const AcroIcon: React.FC<AcroIconProps> = ({ name, spin = false }) => {
+const AcroIcon: React.FC<AcroIconProps> = ({ name, spin = false, className, style, onClick }) => {
   const Icon = iconsMap[name as IconName]
   if (!Icon) {
     console.warn(`Icon with name "${name}" does not exist in iconsMap.`);
     return null
   }
-  return <Icon spin={spin} />
+  return <Icon spin={spin} className={className} style={style} onClick={onClick} />
 }
 
 export default AcroIcon;
